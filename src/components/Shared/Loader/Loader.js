a6cdb33c543089fc632214bc85a66758ca81bef2
Hogor007 <asifpenta@gmail.com>
2022-04-18 23:47:04 +0600

    added shared & skills component

---
+++ src/components/Shared/Loader/Loader.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Loader.css";
+
+const Loader = () => {
+  return (
+    <div>
+      <div className="loader mx-auto my-4">
+      </div>
+    </div>
+  );
+};
+
+export default Loader;
